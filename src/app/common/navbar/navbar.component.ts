3227a1fb24b8c3a5f406101fba9b1ef2bfaa17e8
DESKTOP-15M7SGM\HP <shubhuramtekkar@gmail.com>
2022-11-04 12:57:52 +0530

    add to cart added

--- src/app/common/navbar/navbar.component.ts
+++ src/app/common/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { CommonService } from 'src/app/shared/common.service';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent implements OnInit {
+  cartCount = 0;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private commonService: CommonService) {
+    this.commonService.getClickEvent().subscribe(() => {
+      this.getCartData();
+    });
   }
 
+  ngOnInit(): void {}
+
+  getCartData() {
+    this.cartCount = this.cartCount + 1;
+  }
 }
